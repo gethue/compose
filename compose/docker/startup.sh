a2c0543b2fdcc15bbd1351056d7e91e6e80064de
Romain Rigaux <romain.rigaux@gmail.com>
2021-03-01 10:52:37 -0800

    [docker] Reuse pip package inside the build

--- compose/docker/startup.sh
+++ compose/docker/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python compose/manage.py migrate --noinput
+compose migrate
 
-# Temp demo DB to query
-DJANGO_SUPERUSER_PASSWORD=hue python compose/manage.py createsuperuser --username hue --noinput --email 'hue@gethue.com'
+# Temp hue:hue and demo DB
+DJANGO_SUPERUSER_PASSWORD=hue compose createsuperuser --username hue --noinput --email 'hue@gethue.com'
 cp compose/db.sqlite3 compose/db-demo.sqlite3
 
-gunicorn compose.conf.wsgi --bind 0.0.0.0:8005 --threads 10 --access-logfile '-' --error-logfile '-'
+compose start
