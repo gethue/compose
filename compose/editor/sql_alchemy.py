edbbec9cc4e8f3ce5ed3e99d3ee5664822cc7d15
Romain Rigaux <romain.rigaux@gmail.com>
2021-03-02 09:35:43 -0800

    [editor] Add explain API

--- compose/editor/sql_alchemy.py
+++ compose/editor/sql_alchemy.py
@@ -1,21 +1,4 @@
-#!/usr/bin/env python
 # -- coding: utf-8 --
-# Licensed to Cloudera, Inc. under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  Cloudera, Inc. licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # Licensed to Cloudera, Inc. under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -70,6 +53,7 @@ class AuthenticationRequired(Exception):
 
 class OperationTimeout(Exception):
     def __str__(self):
+
         return "OperationTimeout"
 
 
@@ -126,6 +110,12 @@ class SqlAlchemyApi:
             )
 
     def _get_engine_key(self):
+        return ENGINE_KEY % {
+            "username": self.user.username,
+            "connector_name": self.interpreter["name"],
+        }
+
+    def _get_engine_key2(self):
         return ENGINE_KEY % {
             "username": self.user.username,
             "connector_name": self.interpreter["name"],
