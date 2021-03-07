e9272971253eeab872e5b867ddc0bdd71eb72e32
Romain Rigaux <romain.rigaux@gmail.com>
2021-03-07 07:02:40 -0800

    [editor] Split out execute logic from the API view

--- compose/conf/settings.py
+++ compose/conf/settings.py
@@ -143,8 +143,8 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
-        "rest_framework.parsers.FormParser",
-        "rest_framework.parsers.MultiPartParser",
+        # "rest_framework.parsers.FormParser",
+        # "rest_framework.parsers.MultiPartParser",
     ],
 }
 
